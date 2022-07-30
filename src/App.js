de742acb0b2b5ead7aa9bde848a79303c4a782c8
unknown <tasnimtanzim55@gmail.com>
2022-07-30 23:01:06 +0600

    Notes App created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      title: 'This is my First title',
+      description: 'This is my First Description.This is my First Description.This is my First Description.This is my First Description.'
+    }
+  ])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const description = e.target.description.value;
+    console.log(title, description)
+    setNotes([...notes, { title: title, description: description }])
+  }
+  const handleDelete = (title) => {
+    const filter = notes.filter(note => note.title !== title)
+    setNotes(filter);
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Note App</h1>
+      <div className='notes'>
+          <form className='addNote' onSubmit={handleSubmit}>
+            <input type="text" name='title' placeholder='Title' />
+            <textarea type="text" rows={6} name='description' placeholder='Description' />
+            <input type='submit' value='Save' className='button'/>
+          </form>
+        {
+          notes.map((note, index) =>
+            <div key={index} className='child'>
+              <p style={{ fontWeight: 'bold' }}>{note.title}</p>
+              <p className='description'>{note.description}</p>
+              <button className='button' onClick={() => handleDelete(note.title)}>Delete</button>
+            </div>
+          )
+        }
+      </div>
     </div>
   );
 }
