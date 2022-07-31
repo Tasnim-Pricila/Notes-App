9b01b2685f1b9267919afe43a854b07946f39ea8
unknown <tasnimtanzim55@gmail.com>
2022-07-31 17:41:38 +0600

    DOne

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [notes, setNotes] = useState([
-    {
-      title: 'This is my First title',
-      description: 'This is my First Description.This is my First Description.This is my First Description.This is my First Description.'
-    }
-  ])
+  const [notes, setNotes] = useState([])
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -20,25 +15,27 @@ function App() {
     const filter = notes.filter(note => note.title !== title)
     setNotes(filter);
   }
-  
 
+  console.log(notes)
   return (
     <div>
       <h1>Note App</h1>
       <div className='notes'>
-          <form className='addNote' onSubmit={handleSubmit}>
-            <input type="text" name='title' placeholder='Title' />
-            <textarea type="text" rows={6} name='description' placeholder='Description' />
-            <input type='submit' value='Save' className='button'/>
-          </form>
+        <form className='addNote' onSubmit={handleSubmit}>
+          <input type="text" name='title' placeholder='Title' />
+          <textarea type="text" rows={6} name='description' placeholder='Description' />
+          <input type='submit' value='Save' className='button' />
+        </form>
         {
-          notes.map((note, index) =>
-            <div key={index} className='child'>
-              <p style={{ fontWeight: 'bold' }}>{note.title}</p>
-              <p className='description'>{note.description}</p>
-              <button className='button' onClick={() => handleDelete(note.title)}>Delete</button>
-            </div>
-          )
+          notes.length > 0 ?
+            notes.map((note, index) =>
+              <div key={index} className='child'>
+                <p className='title'>{note.title}</p>
+                <p className='description'>{note.description}</p>
+                <button className='button' onClick={() => handleDelete(note.title)}>Delete</button>
+              </div>
+            )
+            :  ''
         }
       </div>
     </div>
